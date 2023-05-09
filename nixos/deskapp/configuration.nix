7f33822b22ad9847d1784aabaa86a96b9aafa8a5
Andre-Philippe Paquet <appaquet@gmail.com>
2023-05-08 21:55:28 -0400

    update

--- nixos/deskapp/configuration.nix
+++ nixos/deskapp/configuration.nix
@@ -80,8 +80,12 @@
   networking.firewall.enable = false;
 
   services.vscode-server.enable = true;
-  #services.vscode-server.enableFHS = true;
-  #services.vscode-server.extraRuntimeDependencies = with pkgs; [
-  #  zlib
-  #];
+  services.vscode-server.enableFHS = false;
+  services.vscode-server.extraRuntimeDependencies = with pkgs; [
+    zlib
+    glib
+    clang
+    llvmPackages.libclang
+    llvmPackages.libcxxClang
+  ];
 }
--- shells/exocore/flake.nix
+++ shells/exocore/flake.nix
@@ -6,7 +6,8 @@
   description = "exomind";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
@@ -39,14 +40,20 @@
               llvmPackages.libclang
               llvmPackages.libcxxClang
               zlib
+              glib
             ];
 
             NODE_OPTIONS = "--openssl-legacy-provider"; # nodejs SSL error. see https://github.com/NixOS/nixpkgs/issues/209668
 
-            #NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            #  pkgs.stdenv.cc.cc
-            #];
-            #NIX_LD = builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+            NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.clang
+              pkgs.llvmPackages.libclang
+              pkgs.llvmPackages.libcxxClang
+              pkgs.zlib
+              pkgs.glib
+            ];
+            NIX_LD = builtins.readFile "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
           };
         };
       });
