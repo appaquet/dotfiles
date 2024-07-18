2f4c7bed7ae6a19c2c16fb6b4888644c1d183210
Andre-Philippe Paquet <appaquet@gmail.com>
2024-07-17 22:00:29 -0400

    update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs"; # makes home-manager's nixpkgs input follow our nixpkgs version
     };
 
