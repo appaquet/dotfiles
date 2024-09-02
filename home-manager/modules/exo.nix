407771043609214ffe943f6c7bd0b35000f70a86
Andre-Philippe Paquet <appaquet@gmail.com>
2024-09-01 20:27:56 -0400

    update

--- home-manager/modules/exo.nix
+++ home-manager/modules/exo.nix
@@ -1,7 +1,26 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    #exo
+    exo
   ];
+
+  systemd.user.services.exocore = {
+    Unit = {
+      Description = "Exocore daemon";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "watch-store" ''
+        #!/run/current-system/sw/bin/bash
+        cd /mnt/manjaro/home/appaquet/Projects/exomind/exocore
+        ${pkgs.exo}/bin/exo -d local_conf daemon
+      ''}";
+      WorkingDirectory = "/mnt/manjaro/home/appaquet/Projects/exomind/exocore";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
 }
 
--- overlays/exo/default.nix
+++ overlays/exo/default.nix
@@ -8,16 +8,20 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "appaquet";
     repo = "exocore";
-    rev = "68cb916";
-    hash = "sha256-dhog4wiYd5v/WJbjch+IgL6iv1fqwEVtvhedQKEubtg=";
+    rev = "bump-deps";
+    hash = "sha256-fsDu3ng0oppnsi9hDVkhjHVdOIyYECoTx0wKZg4xfCY=";
   };
 
-  cargoHash = "sha256-dhog4wiYd5v/WJbjch+IgL6iv1fqwEVtvhedQKEubtg";
+  cargoHash = "sha256-p/vX+d2LoGNyU1FiJKjYTpsiuz+2zhdiSLMc8mOJADU=";
+
+  cargoBuildFlags = [
+    "--package"
+    "exo"
+  ];
+
+  doCheck = false; # don't run tests
 
   meta = {
     description = "Exocore cli";
-    homepage = "https://github.com/appaquet/exocore";
-    license = lib.licenses.apache20;
-    maintainers = [ ];
   };
 }
