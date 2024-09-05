24bcb6a9e6de6371d3f26b496b1d0542d4a0ea2c
Andre-Philippe Paquet <appaquet@gmail.com>
2024-09-04 22:05:41 -0400

    fix ssh

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -5,43 +5,37 @@
     enable = true;
 
     matchBlocks = {
-      github = {
-        hostname = "github.com";
-        identityFile = "~/.ssh/github_id_rsa";
-      };
-
-      servapp = {
-        hostname = "servapp.n3x.net";
-        user = "appaquet";
+      "servapp.n3x.net" = {
         forwardAgent = true;
       };
 
-      deskapp = {
-        hostname = "deskapp.n3x.net";
-        user = "appaquet";
+      "deskapp.n3x.net" = {
         forwardAgent = true;
       };
 
-      nixos = { # TODO: temp
-        hostname = "nixos.n3x.net";
-        user = "appaquet";
+      "nixos.n3x.net" = { # TODO: temp
         forwardAgent = true;
       };
 
-      exocore1 = {
-        hostname = "exocore1.n3x.net";
+      "exocore1.n3x.net" = {
         user = "root";
         port = 22943;
       };
 
-      pihole = {
-        hostname = "pihole.n3x.net";
+      "pihole.n3x.net" = {
         user = "root";
       };
 
-      pikvm = {
-        hostname = "pikvm.n3x.net";
+      "pikvm.n3x.net" = {
         user = "root";
+      };
+
+      "github.com" = {
+        identityFile = "~/.ssh/github_id_rsa";
+      };
+
+      "*.n3x.net" = {
+        user = "appaquet";
       };
     };
   };
