dac5302674b2b383e168ea66996371ab1b2e307e
Andre-Philippe Paquet <appaquet@gmail.com>
2024-10-01 13:39:56 -0400

    auto updater

--- overlays/exo/default.nix
+++ overlays/exo/default.nix
@@ -3,16 +3,16 @@
 # See https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md
 rustPlatform.buildRustPackage {
   pname = "exo";
-  version = "0.1.25";
+  version = "0.1.26";
 
   src = fetchFromGitHub {
     owner = "appaquet";
     repo = "exocore";
     rev = "v0.1.26";
-    hash = "sha256-SMT5Qal4FkF1MTRDvjWYico6AzH/byqrVUbzpsQiufE=";
+    hash = "sha256-k+jFQotmH4dfDMMxdOhZZCjYgb5dxB8eOmeDmjUK/0Y=";
   };
 
-  cargoHash = "sha256-mmzDZT/XlLeQPl/Fi+0uf2vcr5G8YA49vUVewFTAels=";
+  cargoHash = "sha256-p0WMSHNMlbwHfF0bxaXHNRvg6Z+IyyckxrZ3BzMkzgI=";
 
   cargoBuildFlags = [
     "--package"
