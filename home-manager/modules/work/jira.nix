a4cfaf3f89941d124358817deda29f3f75824eb9
Andre-Philippe Paquet <appaquet@gmail.com>
2024-10-04 12:33:15 -0400

    jira token

--- home-manager/modules/work/jira.nix
+++ home-manager/modules/work/jira.nix
@@ -1,14 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, secrets, ... }:
 
 {
   home.packages = with pkgs; [
     jira-cli-go
   ];
 
-  programs.fish.functions.jira-list = "jira sprint list --current -a(jira me) --plain --columns \"key,summary,status\" --order-by rank --reverse";
+  programs.fish = {
+    functions.jira-list = "jira sprint list --current -a(jira me) --plain --columns \"key,summary,status\" --order-by rank --reverse";
 
-  programs.fish.shellAbbrs = {
-    jb = "jira-board"; # from humanfirst shared
-    jl = "jira-list";
+    shellAbbrs = {
+      jb = "jira-board"; # from humanfirst shared
+      jl = "jira-list";
+    };
   };
+
+  programs.fish.interactiveShellInit = ''
+    set JIRA_API_TOKEN (cat ${secrets.work.jiraToken})
+  '';
 }
