751592de51a1c5d721f2f2071e4ef181cd93af76
Andre-Philippe Paquet <appaquet@gmail.com>
2025-04-21 07:32:05 -0400

    neovim: deprecated fix

--- home-manager/modules/neovim/conf/lang.lua
+++ home-manager/modules/neovim/conf/lang.lua
@@ -31,7 +31,7 @@ lspconfig.lua_ls.setup({
 	on_init = function(client)
 		if client.workspace_folders then
 			local path = client.workspace_folders[1].name
-			if path ~= vim.fn.stdpath("config") and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc")) then
+			if path ~= vim.fn.stdpath("config") and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc")) then
 				return
 			end
 		end
