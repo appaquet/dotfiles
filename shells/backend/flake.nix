a7f2894d5ed1d2e782482e7863ba3d1f6983156d
Andre-Philippe Paquet <appaquet@gmail.com>
2025-05-25 13:51:33 -0400

    revert backend shell

--- shells/backend/flake.nix
+++ shells/backend/flake.nix
@@ -6,7 +6,7 @@
   description = "HF backend";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
