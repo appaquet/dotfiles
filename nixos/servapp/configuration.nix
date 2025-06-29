36aa8b569d1a7202318fa07896de29da0cd19b96
Andre-Philippe Paquet <appaquet@gmail.com>
2025-06-29 09:02:29 -0400

    nixos: deprecation--

--- nixos/servapp/configuration.nix
+++ nixos/servapp/configuration.nix
@@ -79,7 +79,7 @@
   services.displayManager.autoLogin.user = "appaquet";
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
