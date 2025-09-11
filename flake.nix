e46097cd961cee1dbba344edb3748a3ddf7736e3
Andre-Philippe Paquet <appaquet@gmail.com>
2025-09-11 12:29:02 +0000

    flake: back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     # nixpkgs-unstable is used instead of nixos-unstable since it has no guarantee of being cached
     # use nixos-25.05 for nixos systems to have a stable system
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos.url = "github:nixos/nixpkgs/nixos-25.05";
 
     flake-utils.url = "github:numtide/flake-utils";
