be174ebee2a832e3caaa28723f381628ec0f6f32
Andre-Philippe Paquet <appaquet@gmail.com>
2025-10-16 07:27:01 -0400

    claude++

--- overlays/claude-code/default.nix
+++ overlays/claude-code/default.nix
@@ -7,16 +7,16 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "2.0.17";
+  version = "2.0.19";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-uQ+iosjjs6dUT8MtnFwubJqp85pBtBYkmIXTlRGufQw=";
+    hash = "sha256-wUpOZzGrs1VekDt0T+j9t9E+dVPPfjQUJxgqWVU3eGk=";
   };
 
-  npmDepsHash = "sha256-xuie+/6f+4M7PAgwOSBKaTM3240K+bKD8gspxTm0De0=";
+  npmDepsHash = "sha256-yN2Omq1IPQwruW00lLXOkHYljp6WbYZ022/ehzjNXfM=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
