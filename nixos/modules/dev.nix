49e8867bd28e576e863d1ff1ac3e595db688352b
Andre-Philippe Paquet <appaquet@gmail.com>
2025-12-01 22:18:48 -0500

    nixos: deprecation--

--- nixos/modules/dev.nix
+++ nixos/modules/dev.nix
@@ -1,16 +1,12 @@
 {
   pkgs,
-  config,
   ...
 }:
 
 {
-  environment.systemPackages =
-    (with pkgs; [
-      distrobox
-      sysstat # iostat
-    ])
-    ++ [
-      config.boot.kernelPackages.perf # perf, aligned with current kernel version
-    ];
+  environment.systemPackages = with pkgs; [
+    distrobox
+    perf
+    sysstat # iostat
+  ];
 }
