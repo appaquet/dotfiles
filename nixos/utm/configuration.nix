e3e63f91865c77ced6306bf3effe70be34141c0a
Andre-Philippe Paquet <appaquet@gmail.com>
2026-01-25 22:40:31 -0500

    nixos: utm config

--- nixos/utm/configuration.nix
+++ nixos/utm/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -15,6 +15,41 @@
 
   networking.firewall.enable = false;
   services.openssh.enable = true;
+
+  programs.firefox.enable = true;
+
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    # xwayland.enable = true; # Enabled by default, but good to keep in mind
+  };
+
+  # Hint Electon apps to use Wayland
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # Essential system packages
+  environment.systemPackages = with pkgs; [
+    waybar # The status bar
+    dunst # Notification daemon
+    libnotify # Required for dunst
+    swww # Wallpaper daemon (or hyprpaper)
+    rofi # App launcher
+    kitty # Default terminal (Hyprland's default is kitty)
+    networkmanagerapplet # Wifi tray icon
+    #dolphin               # filemanager
+  ];
+
+  # Optional: Enable a Display Manager (SDDM is common for Wayland)
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+
+  # Fonts (Waybar needs these for icons)
+  fonts.packages = with pkgs; [
+    nerd-fonts.symbols-only
+    nerd-fonts.fira-code
+  ];
 
   system.stateVersion = "25.01";
 }
