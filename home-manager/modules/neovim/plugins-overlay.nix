a318d5a4c434124cac4215253ce3fdaece18f61a
Andre-Philippe Paquet <appaquet@gmail.com>
2026-04-02 10:02:31 -0400

    bumps

--- home-manager/modules/neovim/plugins-overlay.nix
+++ home-manager/modules/neovim/plugins-overlay.nix
@@ -5,5 +5,19 @@ final: prev: {
       name = "lsp-notify";
       src = ./plugins/lsp-notify;
     };
+
+    # https://github.com/zbirenbaum/copilot.lua
+    copilot-lua = prev.vimUtils.buildVimPlugin {
+      pname = "copilot.lua";
+      version = "2026-04-02";
+      src = prev.fetchFromGitHub {
+        owner = "zbirenbaum";
+        repo = "copilot.lua";
+        rev = "faa347cef2a9429eec14dada549e000a3b8d0fc9";
+        sha256 = "sha256-K3jeiJGyvT8dNtPNjd2cMwTjAewd9d6NB7akkdTl4Yk=";
+      };
+      meta.homepage = "https://github.com/zbirenbaum/copilot.lua/";
+      meta.hydraPlatforms = [ ];
+    };
   };
 }
