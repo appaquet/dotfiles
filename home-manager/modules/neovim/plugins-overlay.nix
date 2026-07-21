73070092a71348d0106d93c934695a9aef5e5c94
Andre-Philippe Paquet <appaquet@gmail.com>
2026-07-21 16:24:10 -0400

    nvim bumps

--- home-manager/modules/neovim/plugins-overlay.nix
+++ home-manager/modules/neovim/plugins-overlay.nix
@@ -3,12 +3,12 @@ final: prev: {
     # https://github.com/zbirenbaum/copilot.lua
     copilot-lua = prev.vimUtils.buildVimPlugin {
       pname = "copilot.lua";
-      version = "2026-07-04";
+      version = "2026-07-21";
       src = prev.fetchFromGitHub {
         owner = "zbirenbaum";
         repo = "copilot.lua";
-        rev = "d521d3959f81f4fa58870ed329754f1dd5057511";
-        sha256 = "sha256-5n5wogPhLmej9ov62qHFKZMNYhKGiz+zjQwPExGaKTI=";
+        rev = "2a849bc7bec4d175524c332c20d48ca9ece735df";
+        sha256 = "sha256-1tyiXW2O7tOr9jMDRUL2aSqQGMiSFstu6M4J/LeSQaA=";
       };
       meta.homepage = "https://github.com/zbirenbaum/copilot.lua/";
       meta.hydraPlatforms = [ ];
@@ -21,8 +21,8 @@ final: prev: {
       src = prev.fetchFromGitHub {
         owner = "dlyongemallo";
         repo = "diffview-plus.nvim";
-        rev = "424b724e7699730b818466532f0ca42930313273";
-        sha256 = "sha256-tsL0eeTE5F81GzrvvAx0SEH461AYE5H1OWL8PdokshE=";
+        rev = "62dc5adf4e77489a2a6d3bf36ef6e4ac5738b634";
+        sha256 = "sha256-yqFT+Iastcr3YxlqjKtlDzuEvcw7oSLDGAdcEiodvs0=";
       };
       doCheck = false;
       meta.homepage = "https://github.com/dlyongemallo/diffview-plus.nvim/";
