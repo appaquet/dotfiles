f1857fd5ff533bb13e3cdee8b649bca309a4f0b6
Andre-Philippe Paquet <appaquet@gmail.com>
2026-07-22 09:09:09 -0400

    gha nix caching

--- nixos/modules/cachix.nix
+++ nixos/modules/cachix.nix
@@ -1,8 +1,10 @@
 { ... }:
 {
   # used by darwin + nixos
-  # update github workflows too when changing
   # to generate one, `cachix use <cache-name>` and copy the settings from ~/.config/nix/nix.conf
+  #
+  # !! IMPORTANT !!
+  # Update github workflows too when changing
   nix = {
     settings = {
       substituters = [
@@ -13,6 +15,7 @@
         "https://cache.nixos-cuda.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
